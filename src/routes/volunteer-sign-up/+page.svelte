4720102faed38bca446cb7ace2b7722fed85f03d
caleb-goodnite <calebgoodnite@gmail.com>
2025-04-06 01:49:27 -0500

    Removed extra nav from volunteer page

--- src/routes/volunteer-sign-up/+page.svelte
+++ src/routes/volunteer-sign-up/+page.svelte
@@ -49,17 +49,6 @@
   
   <title>Volunteer - Book ReViews</title>
   
-  <nav id="navcont">
-      <div class="btncont">
-      <a href="/"><button id="navbutton">Home</button></a>
-      <a href="/inventory"><button id="navbutton">Inventory</button></a>
-      <a href="/#about"><button id="navbutton">About Us</button></a>
-      <a href="/cart"><button id="navbutton">Cart</button></a>
-      <a href="/checkout"><button id="navbutton">Checkout</button></a>
-      <a href="/volunteer-sign-up"><button id="navbutton">Volunteer</button></a>
-      </div>
-  </nav>
-  
   <div class="volunteer-container">
     <div class="volunteer-header">
       <h1>Volunteer at Book ReViews</h1>
