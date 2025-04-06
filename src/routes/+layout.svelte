bab1ac67e997e462a2e7d47826c39a3b6fb367b5
caleb-goodnite <calebgoodnite@gmail.com>
2025-04-06 01:48:42 -0500

    Changed +layout code to get hamburger menu working. Potentially

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
-  import "../styles.css"; // Change to match your filename
+  import "../styles.css";
   
   let mobileMenuOpen = false;
   
   function toggleMobileMenu() {
     mobileMenuOpen = !mobileMenuOpen;
+    console.log("Menu toggled:", mobileMenuOpen); // For debugging
   }
 </script>
 
@@ -19,7 +20,7 @@
     <a href="/donations"><button id="navbutton">Donate</button></a>
   </div>
   
-  <button class="hamburger-menu" on:click={toggleMobileMenu}>
+  <button class="hamburger-menu" on:click={toggleMobileMenu} aria-label="Toggle navigation menu">
     <span></span>
     <span></span>
     <span></span>
