5b33b6ca001a6191af0859f76f935dfa92375dd4
Caleb-Goodnite <calebgoodnite@gmail.com>
2025-06-17 10:46:29 -0500

    using adapter auto and also removed redirects file

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,10 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // Use Netlify adapter
-    adapter: adapter({
-      // Enable Edge Functions (optional, set to false to use standard serverless functions)
-      edge: false,
-      // If you need to split your app into multiple functions, use the following:
-      // split: true
-    }),
-    // Handle API routes properly
-    files: {
-      lib: 'src/lib',
-      routes: 'src/routes',
-      appTemplate: 'src/app.html',
-      errorTemplate: 'src/error.html'
-    },
+    // Use the auto adapter which automatically chooses the right adapter for your deployment target
+    adapter: adapter(),
     // Prerender settings
     prerender: {
       // This ensures all pages are prerendered when possible
