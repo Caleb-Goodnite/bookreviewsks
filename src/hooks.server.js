280b71ad6ad0522ac02617c9bf9cd3cc6ae31158
caleb-goodnite <calebgoodnite@gmail.com>
2025-07-12 23:54:34 -0500

    angrily made hooks.server.js and realized caches headers anyway so it probably worked but cache hates me now

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,39 +1,37 @@
-// SvelteKit server hooks for security headers
-// Allows Square, Google Fonts, and PocketBase
-
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
     const response = await resolve(event, {
-        filterSerializedResponseHeaders(name) {
-            // Allow all headers to be sent to the client
-            return true;
-        }
+      filterSerializedResponseHeaders(name) {
+        // Send all headers to client
+        return true;
+      }
     });
-
-    // --- Content Security Policy ---
-    // Allow self, Google Fonts, Square, and PocketBase
+  
     response.headers.set(
-        'Content-Security-Policy',
-        [
-            "default-src 'self';",
-            "script-src 'self' https://*.squareup.com https://*.google.com https://*.googletagmanager.com 'unsafe-inline';",
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
-            "font-src 'self' https://fonts.gstatic.com;",
-            "img-src 'self' data: https://*.squarecdn.com https://*.squareup.com https://book-reviews.pockethost.io https://*.googleusercontent.com;",
-            "connect-src 'self' https://book-reviews.pockethost.io https://*.squareup.com https://*.googleapis.com https://*.google.com;",
-            "frame-src 'self' https://*.squareup.com;"
-        ].join(' ')
+      'Content-Security-Policy',
+      [
+        "default-src 'self';",
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.squareup.com https://*.web.squarecdn.com https://*.squarecdn.com https://*.square.com https://*.google.com https://*.googletagmanager.com https://va.vercel-scripts.com;",
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://sandbox.web.squarecdn.com https://*.web.squarecdn.com https://*.squarecdn.com;",
+        "font-src 'self' https://fonts.gstatic.com;",
+        "img-src 'self' data: https://*.squarecdn.com https://*.squareup.com https://*.web.squarecdn.com https://book-reviews.pockethost.io https://*.googleusercontent.com;",
+        "connect-src 'self' https://book-reviews.pockethost.io https://*.squareup.com https://*.web.squarecdn.com https://*.squarecdn.com https://*.square.com https://pci-connect.squareupsandbox.com https://pci-connect.squareup.com https://ingest.sentry.io https://o160250.ingest.sentry.io https://*.googleapis.com https://*.google.com;",
+        "frame-src 'self' https://*.squareup.com https://*.web.squarecdn.com https://*.squarecdn.com https://*.square.com;",
+        "object-src 'none';",
+        "base-uri 'self';"
+      ].join(' ')
     );
-
-    // --- Other security headers ---
+  
+    // Other security headers
     response.headers.set('X-Frame-Options', 'SAMEORIGIN');
     response.headers.set('X-Content-Type-Options', 'nosniff');
     response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
     response.headers.set('Permissions-Policy', 'geolocation=(), microphone=()');
     response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-
-    // Enable HSTS for Vercel deployment (HTTPS only)
-    response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-
+  
+    // Uncomment if your site uses HTTPS and you want HSTS
+    // response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  
     return response;
-}
+  }
+  
